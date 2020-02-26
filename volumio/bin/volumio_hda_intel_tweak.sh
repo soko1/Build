5a2184507e7ff49ff683d74805fe9388f0c30dec
Gé Koerkamp <ge.koerkamp@gmail.com>
2020-02-26 20:08:08 +0100

    HDA Intel tweaks: add HDA Intel PCH/ ALC892

--- volumio/bin/volumio_hda_intel_tweak.sh
+++ volumio/bin/volumio_hda_intel_tweak.sh
@@ -1,20 +1,37 @@
+  GNU nano 3.2                                 /usr/local/bin/volumio_hda_intel_tweak.sh                                            
+
 #!/bin/bash
 
 for card in /sys/class/sound/card*; do
 
-  chip=$(cat /$card/hdaudio*/chip_name)
-  acard=$(cat /proc/asound/cards | grep "$(cat $card/number) \[$(cat $card/id)")
-
-  if [[ $acard =~ "HDA Intel PCH" ]]; then
-    if [ $chip == "ALC283" ]; then
-      echo "Unmute / Mute SPDIF Output for the sound card"
-      echo "-----------------------------"
-      /usr/bin/amixer -c 0 set IEC958,0 mute
-      /usr/bin/amixer -c 0 set 'IEC958 Default PCM',0 mute
-      /usr/bin/amixer -c 0 set IEC958,16 unmute
-    fi
-  fi
+  chip=$(amixer -c 0 info | grep "Mixer name" | awk -F": " '{print (substr($2, 2, length($2) - 2))}')
+  acard=$(cat /proc/asound/cards | grep "$(cat $card/number) \[$(cat $card/id)" | awk -F" - " '{print $2}')
+  case $acard in
+    "HDA Intel PCH" )
+      case $chip in 
+        "Realtek ALC283" )
+          echo "Unmute / Mute SPDIF Output for the sound card"
+          echo "-----------------------------"
+          /usr/bin/amixer -c 0 set IEC958,16 unmute
+          ;;
+        "Realtek ALC892" )
+          /usr/bin/amixer -c 0 set Front,0 mute
+          /usr/bin/amixer -c 0 set Surround,0 mute
+          /usr/bin/amixer -c 0 set Center,0 mute
+          /usr/bin/amixer -c 0 set LFE,0 mute
+          /usr/bin/amixer -c 0 set IEC958,16 unmute
+          ;;
+        "IDT 92HD81B1X5" )
+          ;;
+      esac
+        ;;
+  esac
 done
+
+
+
+
+
 
 
 
