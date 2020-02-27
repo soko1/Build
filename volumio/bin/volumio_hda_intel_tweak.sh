981fe7da3b855276f68e3bbfe8178a0713f65acc
Gé Koerkamp <ge.koerkamp@gmail.com>
2020-02-27 01:41:51 +0100

    volumio_hda_intel_tweak.sh: improved generic card parsing

--- volumio/bin/volumio_hda_intel_tweak.sh
+++ volumio/bin/volumio_hda_intel_tweak.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 for card in /sys/class/sound/card*; do
-
-  chip=$(amixer -c 0 info | grep "Mixer name" | awk -F": " '{print (substr($2, 2, length($2) - 2))}')
-  acard=$(cat /proc/asound/cards | grep "$(cat $card/number) \[$(cat $card/id)" | awk -F" - " '{print $2}')
-  case $acard in
+  cardno=$(cat $card/number)
+  chip=$(amixer -c $cardno info | grep "Mixer name" | awk -F": " '{print (substr($2, 2, length($2) - 2))}')
+  cardname=$(cat /proc/asound/cards | grep "$(cat $card/number) \[$(cat $card/id)" | awk -F" - " '{print $2}')
+  case $cardname in
     "HDA Intel PCH" )
       case $chip in 
         "Realtek ALC283" )
           echo "Unmute / Mute SPDIF Output for the sound card"
           echo "-----------------------------"
-          /usr/bin/amixer -c 0 set IEC958,16 unmute
+          /usr/bin/amixer -c $cardno set IEC958,16 unmute
           ;;
         "Realtek ALC892" )
-          /usr/bin/amixer -c 0 set Front,0 mute
-          /usr/bin/amixer -c 0 set Surround,0 mute
-          /usr/bin/amixer -c 0 set Center,0 mute
-          /usr/bin/amixer -c 0 set LFE,0 mute
-          /usr/bin/amixer -c 0 set IEC958,16 unmute
+          /usr/bin/amixer -c $cardno set Front,0 mute
+          /usr/bin/amixer -c $cardno set Surround,0 mute
+          /usr/bin/amixer -c $cardno set Center,0 mute
+          /usr/bin/amixer -c $cardno set LFE,0 mute
+          /usr/bin/amixer -c $cardno set IEC958,16 unmute
           ;;
         "IDT 92HD81B1X5" )
           ;;
@@ -25,6 +25,7 @@ for card in /sys/class/sound/card*; do
         ;;
   esac
 done
+
 
 
 
